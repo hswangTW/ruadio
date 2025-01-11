e8560dd6851331825417db8a38c6f34f201ae6fc
Hanson <hanson2693@gmail.com>
2025-01-12 01:00:35 +0800

    refactor(python): :recycle: turn the Python module into a Python package
    Because the `filter` submodule didn't have a `__init__.py`, users were
    not able to import the classes inside it with
    `from rustafx.filter import ...`. Now it is a valid Python module with
    `__init__.py`, so users can import the classes inside it with the above
    import statement.

---
+++ python/rustafx/filter/__init__.py
@@ -0,0 +1,10 @@
+from ..rustafx import filter as _filter
+
+__doc__ = _filter.__doc__
+__all__ = [
+    'LinearInterpDelay',
+    'SincInterpDelay',
+]
+
+LinearInterpDelay = _filter.LinearInterpDelay
+SincInterpDelay = _filter.SincInterpDelay
